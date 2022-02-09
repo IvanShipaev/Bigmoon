2d55bba864797c95b81b61f2d5ed22ab5189cc1f
Ivan Shipaev <iva_n_@mail.ru>
2022-02-09 19:58:06 +0800

    Start

---
+++ !build_prj.bat
@@ -0,0 +1,4 @@
+call npm init -y
+call npx hardhat
+call npm install --save-dev hardhat
+call npm install --save-dev @nomiclabs/hardhat-waffle @nomiclabs/hardhat-ethers ethereum-waffle chai  ethers solidity-coverage
